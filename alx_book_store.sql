04f6ec8a9e5f3c56192fae6e7c555d2c786a09d6
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-02 23:12:45 +0300

    updated

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE books(
+CREATE TABLE Books(
     book_id INT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT  Foreign Key REFERENCES Authors,
