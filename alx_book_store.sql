ef982c1c562705ef6759d5f4593ac0bb44d4ce02
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-02 23:04:16 +0300

    added new file

---
+++ alx_book_store.sql
@@ -0,0 +1,36 @@
+CREATE TABLE books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT  Foreign Key REFERENCES Authors,
+    price DECIMAL(10,2),
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+CREATE TABLE Authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE Order_Details(
+    orderdetailid STR PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+)
