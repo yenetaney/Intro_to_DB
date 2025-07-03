74a5def87fab7c7104d61b09e614e81f8e68203b
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-03 22:17:28 +0300

    updated

--- task_2.sql
+++ task_2.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
