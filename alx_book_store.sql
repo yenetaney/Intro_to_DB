acf72e2e11749b8f9f4a5beda7fc5d6b522213bf
Yonatan Haile <yenetaneyh@gmail.com>
2025-07-03 22:11:09 +0300

    updated

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,7 +26,7 @@ CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
